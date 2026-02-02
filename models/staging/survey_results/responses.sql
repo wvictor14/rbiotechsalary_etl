dfa19fe00d2c3e32e435a56a466bd1ab348e74c1
Victor Yuan <victor.2wy@gmail.com>
2026-02-01 22:19:31 -0800

    convert read csv to source

--- models/staging/survey_results/responses.sql
+++ models/staging/survey_results/responses.sql
@@ -1,9 +1,9 @@
 select *
 from
     read_csv_auto(
-        'data/r_biotech salary and company survey - 2025.csv',
+        {{ source("survey_results", "responses") }},
         normalize_names = true,
         sample_size = 100,  -- Scan the WHOLE file to figure out the structure
         max_line_size = 2097152,
         strict_mode = false
-    )
+    ) as src
