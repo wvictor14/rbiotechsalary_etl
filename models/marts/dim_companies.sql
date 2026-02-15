1c4331156a0b2dd111c8a37b3c1ff9f491972f75
Victor Yuan <victor.2wy@gmail.com>
2026-02-15 15:13:45 -0800

    add a unique company_id has composite of several column ids

--- models/marts/dim_companies.sql
+++ models/marts/dim_companies.sql
@@ -1,2 +1,11 @@
-select distinct company_name, company_size, sector, company_is_private_or_public
-from stg_responses
+with
+    distinct_companies as (
+        select distinct company_name, company_size, sector, company_is_private_or_public
+        from {{ ref('stg_responses') }}
+    )
+select
+    {{ dbt_utils.generate_surrogate_key(['company_name', 'company_size', 'sector', 'company_is_private_or_public']) }}
+    as company_id,
+    *
+from distinct_companies
+;
