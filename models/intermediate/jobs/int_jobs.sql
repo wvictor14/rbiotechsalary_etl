38b11dd205919a4fcb08736002af12376b123238
Victor Yuan <victor.2wy@gmail.com>
2026-02-17 22:50:25 -0800

    clean job titles using seed, remove nulls, and filter out rows with job_title_original

--- models/intermediate/jobs/int_jobs.sql
+++ models/intermediate/jobs/int_jobs.sql
@@ -1,9 +1,26 @@
 with
     -- first we apply distinc to columns of interest
     distinct_jobs as (
-        select distinct job_title, job_group from {{ ref('stg_responses') }}
+        select distinct lower(trim(job_title)) as job_title, job_group
+        from {{ ref('stg_responses') }}
+    ),
+    job_titles_mapping as (select * from {{ ref("job_titles") }}),
+    joined as (
+        select
+            l.job_title as job_title_original,
+            r.standardized_job_title as job_title,
+            job_group
+
+        from distinct_jobs as l
+        left join job_titles_mapping as r on l.job_title = r.raw_job_title
+    ),
+    filtered_out_nulls as (
+        select *
+        from joined
+        where job_title_original is not null
+        order by job_title, job_group
     )
 
 select *
-from distinct_jobs
+from filtered_out_nulls
 ;
