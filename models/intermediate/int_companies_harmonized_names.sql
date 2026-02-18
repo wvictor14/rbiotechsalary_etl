af86ad7e0962802b5eb5e762c73c456ac2f58755
Victor Yuan <victor.2wy@gmail.com>
2026-02-17 20:56:47 -0800

    remove unused cte

--- models/intermediate/int_companies_harmonized_names.sql
+++ models/intermediate/int_companies_harmonized_names.sql
@@ -14,17 +14,6 @@ with
             l.company_is_private_or_public
         from distinct_companies as l
         left join {{ ref('company_names') }} as r on l.company_name = r.raw_company_name
-    ),
-    -- then for each company with duplicates we need to decide what metadata is correct
-    -- use the "most selected" and "most recent" tie-breakers
-    -- then we compute a hash based on name, size, and sector
-    company_with_ids as (
-        select
-            {{ dbt_utils.generate_surrogate_key(['company_name', 'company_size', 'sector', 'company_is_private_or_public']) }}
-            as company_id,
-            *
-        from company_names_harmonized
-        order by company_is_private_or_public desc, company_name asc
     )
 select *
 from company_names_harmonized
