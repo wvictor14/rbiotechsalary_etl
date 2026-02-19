628a5885f27aa5cdc19601697531824cf509084d
Victor Yuan <victor.2wy@gmail.com>
2026-02-18 22:49:50 -0800

    use priority to decide tie-breakers on multiple key word matches

--- models/intermediate/jobs/int_jobs.sql
+++ models/intermediate/jobs/int_jobs.sql
@@ -8,7 +8,16 @@ with
     -- map original titles to harmonized titles and job hierarchies
     job_titles_mapping as (select * from {{ ref("job_titles") }}),
     standard_titles as (
-        select l.raw_title, nullif(r.clean_job_title, 'Unknown') as title, department
+        select
+            l.raw_title,
+            nullif(r.clean_job_title, 'Unknown') as title,
+
+            -- Remove commas and slashes to help the fuzzy join match words
+            regexp_replace(
+                nullif(r.clean_job_title, 'Unknown'), '[,/]', ' '
+            ) as title_to_match_on,
+
+            department
 
         from distinct_jobs as l
         left join job_titles_mapping as r on l.raw_title = r.raw_job_title
@@ -33,14 +42,28 @@ with
 
             -- we need to capture all the matches and rank them by priority
             row_number() over (
-                partition by l.title order by r.priority asc
+                partition by l.raw_title
+                order by
+                    r.priority asc,
+
+                    -- longer keywords more specific should be tie-breaker
+                    length(r.keyword) desc
             ) as match_rank
         from removed_nulls as l
-        left join job_levels as r on lower(l.title) like "%" || r.keyword || "%"
+        left join
+            job_levels as r
+            on (
+                -- Using TRIM and LOWER on both sides to be safe
+                trim(lower(l.title_to_match_on)) = trim(lower(r.keyword))
+                or trim(lower(l.title_to_match_on)) like trim(lower(r.keyword)) || ' %'
+                or trim(lower(l.title_to_match_on)) like '% ' || trim(lower(r.keyword))
+                or trim(lower(l.title_to_match_on))
+                like '% ' || trim(lower(r.keyword)) || ' %'
+            )
     ),
     -- take the highest rank (1) 
     job_hierarchy_distinct as (select * from job_hierarchy_mapped where match_rank = 1)
 
-select raw_title, title, standardized_title, hierarchy, department
+select raw_title, title, standardized_title, hierarchy, priority, department
 from job_hierarchy_distinct
 ;
