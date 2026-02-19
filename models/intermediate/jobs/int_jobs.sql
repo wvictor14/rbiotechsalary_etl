432beecdba8f24ca07e9f57a89210ff1afdc171b
Victor Yuan <victor.2wy@gmail.com>
2026-02-18 22:20:21 -0800

    add hierarchy and standardized_title based on seed

--- models/intermediate/jobs/int_jobs.sql
+++ models/intermediate/jobs/int_jobs.sql
@@ -1,37 +1,46 @@
 with
     -- first we apply distinc to columns of interest
     distinct_jobs as (
-        select distinct lower(trim(job_title)) as job_title, job_group
+        select distinct lower(trim(job_title)) as raw_title, job_group as department
         from {{ ref('stg_responses') }}
     ),
 
     -- map original titles to harmonized titles and job hierarchies
     job_titles_mapping as (select * from {{ ref("job_titles") }}),
-    joined as (
-        select
-            l.job_title as job_title_original,
-            r.standardized_job_title as job_title,
-            r.job_level,
-            r.job_hierarchy,
-            job_group
+    standard_titles as (
+        select l.raw_title, nullif(r.clean_job_title, 'Unknown') as title, department
 
         from distinct_jobs as l
-        left join job_titles_mapping as r on l.job_title = r.raw_job_title
+        left join job_titles_mapping as r on l.raw_title = r.raw_job_title
     ),
 
-    -- unknowns should be converted to explicit nulls 
-    filtered_out_nulls as (
-        select
-            job_title_original,
-            nullif(job_title, 'Unknown') as job_title,
-            job_level,
-            job_hierarchy,
-            job_group
-        from joined
-        where job_title_original is not null and job_title != '-'
-        order by job_title, job_group
-    )
+    removed_nulls as (
+        select * from standard_titles where raw_title is not null and title != '-'
+    ),
 
-select *
-from filtered_out_nulls
+    -- map job hierarchy and job level
+    job_levels as (select * from {{ ref("job_hierarchy") }}),
+
+    -- USE A FUZZY JOIN, THE RAW KEYWORD IS A PATTERN TO JOIN ON
+    job_hierarchy_mapped as (
+        select
+            l.raw_title,
+            l.title,
+            r.standardized_title,
+            l.department,
+            r.hierarchy,
+            r.priority,
+
+            -- we need to capture all the matches and rank them by priority
+            row_number() over (
+                partition by l.title order by r.priority asc
+            ) as match_rank
+        from removed_nulls as l
+        left join job_levels as r on lower(l.title) like "%" || r.keyword || "%"
+    ),
+    -- take the highest rank (1) 
+    job_hierarchy_distinct as (select * from job_hierarchy_mapped where match_rank = 1)
+
+select raw_title, title, standardized_title, hierarchy, department
+from job_hierarchy_distinct
 ;
