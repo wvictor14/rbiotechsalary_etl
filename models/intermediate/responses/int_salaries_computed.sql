3a9f4f4839c8971b53a7f86b3a9e50e679c44c13
Victor Yuan <victor.2wy@gmail.com>
2026-02-22 15:04:50 -0800

    bonus should be between 0-100

--- models/intermediate/responses/int_salaries_computed.sql
+++ models/intermediate/responses/int_salaries_computed.sql
@@ -1,5 +1,5 @@
 select
     *,
     base_salary * bonus_pct as bonus_amount,
-    base_salary + (base_salary * bonus_pct) as total_compensation
+    base_salary + (base_salary * bonus_pct / 100) as total_compensation
 from {{ ref("int_salaries_cleaned") }}
