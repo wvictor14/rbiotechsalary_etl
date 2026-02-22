a69fb538d84d013dc3de0b8640f525020fd141b7
Victor Yuan <victor.2wy@gmail.com>
2026-02-21 21:53:31 -0800

    int_responses take raw count city and company name

--- models/intermediate/_int_responses.sql
+++ models/intermediate/_int_responses.sql
@@ -1,15 +1,14 @@
 select
     timestamp,
-    email_address,
 
     -- joining to int_jobs on raw_job_title and raw_department
     raw_job_title,
     raw_department,
 
     -- joining on location
-    country,
-    city,
+    raw_country,
+    raw_city,
 
     -- joining on company
-    company_name
+    raw_company_name
 from {{ ref("stg_responses") }}
