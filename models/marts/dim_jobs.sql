e7a397a74fc3936297f3d101e057bf39ab222cc3
Victor Yuan <victor.2wy@gmail.com>
2026-02-21 21:30:52 -0800

    raw_title carried through to int_jobs, and dropped from dim to maintain grain in dim_jobs

--- models/marts/dim_jobs.sql
+++ models/marts/dim_jobs.sql
@@ -1,30 +1,25 @@
 with
-    dim_job_titles as (select * from {{ ref('dim_job_titles') }}),
+    dim_job_titles as (select * from {{ ref("dim_job_titles") }}),
 
     -- First get distinct combinations of all job attributes
     distinct_job_combos as (
         select distinct
-            raw_job_title,
-            raw_department,
-            title,
-            standardized_title,
-            hierarchy,
-            priority
-        from {{ ref('int_jobs') }}
+            raw_department as department, title, standardized_title, hierarchy, priority
+        from {{ ref("int_jobs") }}
     ),
 
     final as (
         select
-
-            r.raw_job_title,
-            r.raw_department,
-
             -- Generate a surrogate key for the job based on all relevant attributes
-            {{ dbt_utils.generate_surrogate_key(['l.title_id', 'r.raw_department', 'r.hierarchy', 'r.priority']) }}
-            as job_id,
+            {{
+                dbt_utils.generate_surrogate_key(
+                    ["l.title_id", "r.department", "r.hierarchy", "r.priority"]
+                )
+            }} as job_id,
             l.title_id,
             l.title,
             r.standardized_title,
+            r.department,
             r.hierarchy,
             r.priority
         from dim_job_titles as l
@@ -33,5 +28,5 @@ with
 
 select *
 from final
-order by hierarchy, title, raw_department
+order by hierarchy, title, department
 ;
