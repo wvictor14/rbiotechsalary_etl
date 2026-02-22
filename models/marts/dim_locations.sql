d02d7afbaa64832725287c75c688f810607ccd98
Victor Yuan <victor.2wy@gmail.com>
2026-02-21 21:54:22 -0800

    apply a distinct to dim_locations

--- models/marts/dim_locations.sql
+++ models/marts/dim_locations.sql
@@ -1,9 +1,9 @@
-select
+select distinct
     -- Generate a surrogate key for the job based on all relevant attributes
-    {{ dbt_utils.generate_surrogate_key(['country', 'city', 'subdivision']) }}
+    {{ dbt_utils.generate_surrogate_key(["country", "city", "subdivision"]) }}
     as location_id,
     country,
     subdivision,
     city,
     location_name
-from {{ ref('int_locations') }}
+from {{ ref("int_locations") }}
