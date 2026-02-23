cff8114fa218a149c2aeeab3ae576fa8a73c8fa3
Victor Yuan <victor.2wy@gmail.com>
2026-02-22 21:50:22 -0800

    add in salaries to int_responses

--- models/intermediate/int_responses.sql
+++ models/intermediate/int_responses.sql
@@ -11,13 +11,30 @@ with
 
     -- join the foreign keys from other int tables
     joined as (
-        select s.response_id, c.company_name, j.job_id, l.location_id
+        select
+            s.response_id,
+            c.company_name,
+            j.job_id,
+            l.location_id,
+            sa.base_salary,
+            sa.bonus_pct,
+            sa.bonus_amount,
+            sa.total_compensation
         from stg as s
         left join companies as c on s.response_id = c.response_id
         left join jobs as j on s.response_id = j.response_id
         left join locations as l on s.response_id = l.response_id
+        left join salaries as sa on s.response_id = sa.response_id
     )
 
-select *
+select
+    response_id,
+    company_name,
+    job_id,
+    location_id,
+    base_salary,
+    bonus_pct,
+    bonus_amount,
+    total_compensation
 from joined
 ;
